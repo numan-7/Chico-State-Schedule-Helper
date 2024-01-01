5c1774197e7b4aa31d94a56f217380b569ee80d4
Numan Khan <101899800+numan-7@users.noreply.github.com>
2023-12-31 18:21:06 -0800

    Added host permissions to manifest

--- build/popup.js
+++ build/popup.js
@@ -1,18 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/popup.js ***!
-  \**********************/
-document.getElementById('startButton').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ request: 'startSearch' });
-    document.getElementById('btn-span').textContent = 'searching...';
-    document.getElementById('startButton').disabled = true;
-    setTimeout(() => {
-        document.getElementById('startButton').disabled = false;
-        document.getElementById('btn-span').textContent = 'get ratings';
-    }, 2500)
-});
-  
-/******/ })()
-;
-//# sourceMappingURL=popup.js.map
+document.getElementById("startButton").addEventListener("click",(()=>{chrome.runtime.sendMessage({request:"startSearch"}),document.getElementById("btn-span").textContent="searching...",document.getElementById("startButton").disabled=!0,setTimeout((()=>{document.getElementById("startButton").disabled=!1,document.getElementById("btn-span").textContent="get ratings"}),2500)}));
